9c625e70d5ad2c85b213756e545379e046358938
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-25 00:14:47 +0100

    configure router with routerchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
-import {PostsComponent} from './posts/posts.component';
+
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -13,9 +13,7 @@ const appRoutes: Routes = [
   {
     path:'',redirectTo:'/posts',pathMatch:'full'
   },
-  {
-    path: 'posts', component: PostsComponent
-  },
+
 
   {
     path: 'about', component: AboutComponent
@@ -31,7 +29,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     NavBarComponent,
-    PostsComponent,
     AboutComponent,
     PageNotFoundComponent
   ],
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PostListComponent} from './post-list/post-list.component';
 
 const routes: Routes = [
-
+  {path: 'posts', component: PostListComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PostRoutingModule { }
+export class PostRoutingModule {
+}
