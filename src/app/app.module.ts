f9146eecc9df9eb091101bea88683b7ca7beedcc
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-24 22:47:13 +0100

    configuer Router ang generate about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { PostsComponent } from './posts/posts.component';
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {PostsComponent} from './posts/posts.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'posts', component: PostsComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    PostsComponent
+    PostsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
