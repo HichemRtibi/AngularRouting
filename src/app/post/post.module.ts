49c018cc2fdd44dfdbd20f9e7763f5b0e86c7d29
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-25 00:08:26 +0100

    generate post list

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostRoutingModule } from './post-routing.module';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PostListComponent],
   imports: [
     CommonModule,
     PostRoutingModule
